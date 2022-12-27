f1630b7fad86f3cebf5bd87e5b266dd35e6c5e88
AntaninaShabeka <zemlianika@gmail.com>
2022-12-27 16:42:42 +0300

    Тренировка и прогнозирование

---
+++ DiamondsPredicting.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as snb
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('p1_diamonds.csv')
+
+# print(df.head(10).to_string())
+
+# Удаление Unnamed столбцаб axis = 1 означает, что мы удаляем столбец
+df = df.drop(['Unnamed: 0'], axis = 1)
+
+# Создание переменных для категорий
+categorical_features = ['cut', 'color', 'clarity']
+le = LabelEncoder()
+
+# Замена категорий на численные значения
+for i in range(3):
+    new = le.fit_transform(df[categorical_features[i]])
+    df[categorical_features[i]] = new
+
+# print(df.head(10).to_string())
+x = df[['carat','cut','color','clarity','depth','table','x','y','z']]
+y = df[['price']]
+
+# Разделение данных на тренировочный и тестовый наборы
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 25, random_state = 356)
+
+# Тренировка
+regr = RandomForestRegressor(n_estimators = 10, max_depth = 10, random_state = 356)
+regr.fit(x_train, y_train.values.ravel())
+
+# Прогнозирование
+predictions = regr.predict(x_test)
+
+result = x_test
+result['price'] = y_test
+result['prediction'] = predictions.tolist()
+
+print(result.to_string())
